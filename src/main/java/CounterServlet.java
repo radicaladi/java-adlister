2f557ad92083b54fa6646a175a117421fe0bd236
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-30 08:11:49 -0700

    add counter to servlet page

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class CounterServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/counter")
+
+public class CounterServlet extends HttpServlet {
+    public int counter = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        counter++;
+
+        response.getWriter().println("<h1>The count currently is " + counter + " on this page load.</h1>");
+
+    }
 }

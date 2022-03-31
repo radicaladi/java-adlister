c5d6832606cb1fff98ba4d217de4792a281f3bb2
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-31 09:35:15 -0700

    add login servlet logic

--- src/main/java/LoginValidate.java
+++ src/main/java/LoginValidate.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class LoginValidate {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginValidate", urlPatterns = "/loginValidate")
+public class LoginValidate extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userName = request.getParameter("username");
+        String passWord = request.getParameter("password");
+
+        if (userName.equals("admin") && passWord.equals("password")) response.sendRedirect("profile.jsp");
+        if (!userName.equals("admin") && passWord.equals("password")) response.sendRedirect("login.jsp");
+
+    }
 }

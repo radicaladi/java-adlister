c4dd5f213fffa41ba67fd37d7d0155bc4c2ed419
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-31 10:12:55 -0700

    add changes to logic; include error message

--- src/main/java/LoginValidate.java
+++ src/main/java/LoginValidate.java
@@ -1,3 +1,5 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -6,12 +8,18 @@ import java.io.IOException;
 
 @WebServlet(name = "LoginValidate", urlPatterns = "/loginValidate")
 public class LoginValidate extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String userName = request.getParameter("username");
         String passWord = request.getParameter("password");
 
-        if (userName.equals("admin") && passWord.equals("password")) response.sendRedirect("profile.jsp");
-        if (!userName.equals("admin") && passWord.equals("password")) response.sendRedirect("login.jsp");
+        if (userName.equals("admin") && passWord.equals("password")) {
+            response.sendRedirect("profile.jsp");
+        }
+       else {
+           RequestDispatcher rd= request.getRequestDispatcher("login.jsp");
+           request.setAttribute("error","Invalid Username or Password");
+           rd.include(request, response);
+        }
 
     }
 }

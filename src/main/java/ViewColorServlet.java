73114dc59732e30323d26e6504bd4bde459ff127
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-01 13:10:07 -0700

    add changes; set attribute, redirect

--- src/main/java/ViewColorServlet.java
+++ src/main/java/ViewColorServlet.java
@@ -1,2 +1,19 @@
-public class ViewColorServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ViewColorServlet", urlPatterns = "/viewcolor")
+public class ViewColorServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String color = request.getParameter("colorinput");
+
+        request.setAttribute("colorinput", color);
+        request.getRequestDispatcher("viewcolor.jsp").forward(request, response);
+
+    }
+
 }

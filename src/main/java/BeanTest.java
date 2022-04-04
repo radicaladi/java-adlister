34c2970f03f301bd633da31a615ea526f727bfd2
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-04 09:46:37 -0700

    add arraylist quotes and print loop

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -21,13 +22,33 @@ public class BeanTest {
         author1.setLastName("Twain");
         System.out.printf("author1 = %s %s %n", author1.getFirstName(), author1.getLastName());
 
+        Author author2 = new Author();
+        author2.setId(3);
+        author2.setFirstName("John");
+        author2.setLastName("Locke");
+        System.out.printf("author2 = %s %s %n", author2.getFirstName(), author2.getLastName());
+
         Quote author1Q = new Quote();
         author1Q.setId(3);
         author1Q.setContent("Clothes make the man. Naked people have little or no influence on society");
         author1Q.setAuthor(author1);
         System.out.println("Mark Twain: " + author1Q.getContent());
 
+        Quote author2Q = new Quote();
+        author2Q.setId(4);
+        author2Q.setContent("Reading furnishes the mind only with materials of knowledge; it is thinking that makes what we read ours.");
+        author2Q.setAuthor(author2);
+        System.out.println("John Locke: " + author2Q.getContent());
 
+
+        ArrayList<Quote> quoteArrayList = new ArrayList<>();
+
+        quoteArrayList.add(author1Q);
+        quoteArrayList.add(author2Q);
+
+        for (Quote quote : quoteArrayList) {
+            System.out.printf("%s %s once said: %s %n", author1Q.getAuthor().getFirstName(), author1Q.getAuthor().getLastName(), quote.getContent());
+        }
     }
 
 }

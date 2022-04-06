fcd15ffb522593386b0ba361502d822fe0ff9b76
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-06 15:48:45 -0700

    major refactor for organization

---
+++ src/main/java/jdbc/Config.java
@@ -0,0 +1,13 @@
+package jdbc;
+
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+    public String getUser() {
+        return "adlister_user";
+    }
+    public String getPassword() {
+        return "password";
+    }
+}
